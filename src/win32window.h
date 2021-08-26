d4f52cef5f303242253e5c2de7bde668d4218ee0
ousttrue <ousttrue@gmail.com>
2021-08-26 14:59:04 +0900

    win32window.h

---
+++ src/win32window.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <stdint.h>
+
+class Win32Window
+{
+    void *_instance = nullptr;
+    void *_hwnd = nullptr;
+    std::wstring _class_name;
+
+public:
+    ~Win32Window();
+
+    void *Create(void *instance, const wchar_t *class_name,
+                 const wchar_t *window_title);
+
+    uint64_t Proc(void *hwnd, uint32_t msg, uint64_t wparam, uint64_t lparam);
+};
